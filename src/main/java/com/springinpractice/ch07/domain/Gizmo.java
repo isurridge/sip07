20344807f2cc30dd9289460a7f4fd4b51d61e23a
isurridge <isurridge@gmail.com>
2013-07-28 22:51:57 -0700

    adding json

---
+++ src/main/java/com/springinpractice/ch07/domain/Gizmo.java
@@ -0,0 +1,35 @@
+package com.springinpractice.ch07.domain;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.io.Serializable;
+
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.ANY, fieldVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.ANY)
+public class Gizmo implements Serializable {
+    private Long id;
+    private String description;
+
+    public Gizmo(){}
+
+    public Gizmo(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/springinpractice/ch07/web/ExampleGizmoController.java
@@ -0,0 +1,53 @@
+package com.springinpractice.ch07.web;
+
+import com.springinpractice.ch07.domain.Gizmo;
+import com.springinpractice.ch07.domain.ManyGizmoResponse;
+import com.springinpractice.ch07.domain.SingleGizmoResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ExampleGizmoController {
+
+    @RequestMapping(value="/gizmos/{gizmoId}", method= RequestMethod.GET, produces="application/json")
+    @ResponseBody
+    public SingleGizmoResponse getFact(@PathVariable Long gizmoId) throws IOException {
+        System.out.println("Gizmo retrieved.");
+        return new SingleGizmoResponse(true,new Gizmo(1L,"HELLO"));
+    }
+
+    @RequestMapping(value="/gizmos", method= RequestMethod.GET, produces="application/json")
+    @ResponseBody
+    public ManyGizmoResponse getFacts() throws IOException {
+        System.out.println("Gizmo search.");
+        List<Gizmo> gizmos = new ArrayList<Gizmo>();
+        gizmos.add(new Gizmo(1L, "HELLO"));
+        gizmos.add(new Gizmo(2L, "WORLD"));
+        return new ManyGizmoResponse(true,gizmos);
+    }
+
+    @RequestMapping(value="/gizmos", method= RequestMethod.POST, produces="application/json", consumes="application/json")
+    @ResponseBody
+    public SingleGizmoResponse createGizmo(@RequestBody Gizmo gizmo) throws IOException {
+        System.out.println("Gizmo created.");
+        return new SingleGizmoResponse(true,new Gizmo(1L,"NEW GIZMO"));
+    }
+
+    @RequestMapping(value="/gizmos/{gizmoId}", method= RequestMethod.PUT, produces="application/json", consumes="application/json")
+    @ResponseBody
+    public SingleGizmoResponse updateGizmo(@PathVariable Long gizmoId, @RequestBody Gizmo gizmo) throws IOException {
+        System.out.println("Gizmo updated.");
+        return new SingleGizmoResponse(true,new Gizmo(1L,"UPDATED GIZMO"));
+    }
+
+    @RequestMapping(value="/gizmos{gizmoId}", method= RequestMethod.DELETE, produces="application/json"    )
+    @ResponseBody
+    public SingleGizmoResponse deleteGizmo(@PathVariable Long gizmoId) throws IOException {
+        System.out.println("Gizmo deleted.");
+        return new SingleGizmoResponse(true,new Gizmo(gizmoId,null));
+    }
+}
